8dbd578a0217f46991a7ddf82a1c59dcf2cc36e7
kiendao515 <daotrungkien515@gmail.com>
2022-04-17 14:14:59 +0700

    dao

--- 6.js
+++ 6.js
@@ -1,17 +1,10 @@
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer');
 
-(async()=>{
-  browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-  
-  let page = await browser.newPage();
-  
-  await page.goto(event.url || 'https://example.com');
-  
-  result = await page.title();
-})()
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
