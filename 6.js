0d2e0d3c5616d8ee03ac8a1fc53b641babff19c4
kiendao515 <daotrungkien515@gmail.com>
2022-04-17 14:19:55 +0700

    kiennn

--- 6.js
+++ 6.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true,
+    args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
